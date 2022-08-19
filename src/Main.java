fdd46ba5e415706516ff32106b18461434e84eff
Bruce Her <bruce.k.her@gmail.com>
2022-08-19 11:52:31 -0500

    fixed Main file

--- src/Main.java
+++ src/Main.java
@@ -59,3 +59,50 @@ public class Main {
     }
 }
 
+class Cupcake {
+    public double price;
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void type(){
+        System.out.println("A basic, generic cupcake, with vanilla frosting");
+    }
+}
+
+class RedVelvet extends Cupcake{
+    public double price;
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void type(){
+        System.out.println("A red velvet based cupcake, with cream cheese frosting");
+    }
+}
+
+class Chocolate extends Cupcake{
+    public double price;
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void type(){
+        System.out.println("A chocolate based cupcake, with chocolate frosting");
+    }
+}
